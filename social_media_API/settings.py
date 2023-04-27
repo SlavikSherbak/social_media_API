18e49ad6503295226c0721458cf6cfdbc8564f4d
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-04-27 11:27:53 +0300

    update settings.py added installed apps & update user model

--- social_media_API/settings.py
+++ social_media_API/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -80,6 +84,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Password validation
